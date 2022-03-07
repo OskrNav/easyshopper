c44f257234303199a55248163eab87ff63c78acb
Oscar Navidad <onavidad@applaudostudios.com>
2022-03-07 13:13:55 -0600

    initial commit

---
+++ easyShopper/easyShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyShopperApp.swift
+//  easyShopper
+//
+//  Created by Oscar Navidad on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct easyShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
